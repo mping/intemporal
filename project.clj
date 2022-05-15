7150b92e8d4cfb4cfe7bf9301c8d5edfccb4ebd5
Miguel Ping <miguel.ping@gmail.com>
2022-05-15 21:23:20 +0100

    dont use provided scope for logging lib

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.logging "1.2.4" :scope "provided"]]
+                 [org.clojure/tools.logging "1.2.4"]]
   :java-source-paths ["java"]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}}
+                   :dependencies [[ch.qos.logback/logback-classic "1.2.11"]]}}
   :repl-options {:init-ns intemporal.workflow})
