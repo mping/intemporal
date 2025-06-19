be3b6ae4fd4bbc1343a2712204edd70a7a55dc59
Miguel Ping <miguel.ping@gmail.com>
2025-06-19 22:43:16 +0100

    refactor

--- dev/dev/scheduler.clj
+++ dev/dev/scheduler.clj
@@ -1,84 +1,91 @@
 (ns dev.scheduler
   (:require [promesa.core :as p])
-  (:import [java.util.concurrent.locks Condition Lock ReentrantLock]))
+  (:import [java.util.concurrent Executors]
+           [java.util.concurrent.locks Condition Lock ReentrantLock]))
 
 
 ;; Define a Scheduler protocol
 (defprotocol IScheduler
-  (register-thread [this fn])
-  (yield [this thread-id])
-  (run-task [this thread-id task]))
+  (enqueue-task [this fn])
+  (run-task [this task-id fn])
+  (yield [this task-id]))
 
-(def ^:dynamic *thread-id* nil)
-
-(defn self-id []
-  *thread-id*)
+(def ^:dynamic *task-id* nil)
+(defn current-id [] *task-id*)
 
 ;; Scheduler implementation
 (defn make-scheduler []
-  (let [lock           (ReentrantLock.)
-        condition-map  (atom (sorted-map))
-        next-thread-id (atom -1)
-        current-thread (atom 0)]
+  (let [lock            (ReentrantLock.)
+        task-conditions (atom [])
+        next-task-id    (atom -1)
+        current-thread  (atom 0)]
 
     (reify IScheduler
       ;; Register thread and assign it a unique ID
-      (register-thread [this fn]
-        (.lock lock)
-        (try
-          (let [id   (swap! next-thread-id inc)
-                cond (.newCondition lock)]
-            (swap! condition-map assoc id cond)
-
-            (-> (Thread. (reify Runnable
-                           (run [_]
-                             (run-task this id fn))))
-                (.start)))
-          (finally
-            (.unlock lock))))
-
-      (yield [this thread-id]
-        (.lock lock)
-        (try
-          ;; Find next thread
-          (let [ids     (keys @condition-map)
-                idx     thread-id
-                next-id (nth ids (mod (inc idx) (count ids)))]
-            (reset! current-thread next-id)
-            (.signal ^Condition (@condition-map next-id))
-            ;; Suspend current thread
-            (while (not= thread-id @current-thread)
-              (.await ^Condition (@condition-map thread-id))))
-          (finally
-            (.unlock lock))))
+      (enqueue-task [this task]
+        (let [result (p/deferred)]
+          (.lock lock)
+          (try
+            (let [id   (swap! next-task-id inc)
+                  cond (.newCondition lock)]
+              (swap! task-conditions conj cond)
+              (-> (Thread. (reify Runnable
+                             (run [_]
+                               (run-task this id (fn []
+                                                   (try
+                                                     (p/resolve! result (task))
+                                                     (catch Exception e
+                                                       (p/reject! result e))))))))
+                  (.start)))
+            (finally
+              (.unlock lock)))
+          result))
 
       ;; Run a task cooperatively
-      (run-task [this thread-id task]
+      (run-task [this task-id task]
         (.lock lock)
         (try
           ;; Wait until it's this thread's turn
-          (while (not= thread-id @current-thread)
-            (.await ^Condition (@condition-map thread-id)))
+          (while (not= task-id @current-thread)
+            (.await ^Condition (nth task-conditions task-id)))
 
           ;; Run the task
-          (with-bindings {#'*thread-id* thread-id}
+          (with-bindings {#'*task-id* task-id}
             (task))
 
           ;; Determine next thread ID in round-robin order
-          (let [ids     (sort (keys @condition-map))
-                idx     thread-id
-                next-id (nth ids (mod (inc idx) (count ids)))]
+          (let [ids     @task-conditions
+                idx     task-id
+                next-id (mod (inc idx) (count ids))]
             (reset! current-thread next-id)
-            (.signal ^Condition (@condition-map next-id)))
+            (.signal ^Condition (get @task-conditions next-id)))
+          (finally
+            (.unlock lock))))
+
+      (yield [this task-id]
+        (.lock lock)
+        (try
+          ;; Find next thread
+          (let [ids     @task-conditions
+                idx     task-id
+                next-id (mod (inc idx) (count ids))]
+            (reset! current-thread next-id)
+            (.signal ^Condition (get @task-conditions next-id))
+            ;; Suspend current thread
+            (while (not= task-id @current-thread)
+              (.await ^Condition (get @task-conditions task-id))))
           (finally
             (.unlock lock)))))))
 
 
+
 (let [scheduler (make-scheduler)]
   ;; Start initial threads. Thread 1 will spawn another.
-  (register-thread scheduler
-                   (fn []
-                     (println "task 1 with id" (self-id))
-                     (register-thread scheduler (fn [] (println "task 2 with id" (self-id))))
-                     (yield scheduler (self-id))
-                     (println "task 1 finish"))))
+  (println "xxx"
+           @(enqueue-task scheduler
+                          (fn []
+                            (println "task 1 with id" (current-id))
+                            (let [res (enqueue-task scheduler (fn [] (println "task 2 with id" (current-id)) :fin))]
+                              (yield scheduler (current-id))
+                              (println "result?" @res))
+                            (println "task 1 finish")))))
