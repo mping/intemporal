2543164b832a0e00666171d0d7984610f3d6eb9a
Miguel Ping <miguel.ping@gmail.com>
2025-06-21 14:04:44 +0100

    core.async example

--- dev/dev/scheduler.clj
+++ dev/dev/scheduler.clj
@@ -1,6 +1,10 @@
 (ns dev.scheduler
-  (:require [promesa.core :as p])
-  (:import [java.util.concurrent Executors]
+  (:require [promesa.core :as p]
+            [clojure.core.async :as a :refer [>! >!! <! <!! go go-loop chan alts!]])
+  (:require [clojure.core.async.impl.protocols :as protocols]
+            [clojure.core.async.impl.dispatch :as dispatch])
+  (:import [com.google.common.util.concurrent ThreadFactoryBuilder]
+           [java.util.concurrent Executors]
            [java.util.concurrent.locks Condition Lock ReentrantLock]))
 
 
@@ -78,14 +82,118 @@
             (.unlock lock)))))))
 
 
+#_(let [scheduler (make-scheduler)]
+    ;; Start initial threads. Thread 1 will spawn another.
+    (enqueue-task scheduler
+                  (fn []
+                    (println "task 1 with id" (current-id))
+                    (let [res (enqueue-task scheduler (fn [] (println "task 2 with id" (current-id)) :fin))]
+                      (yield scheduler (current-id))
+                      (println "result?" @res))
+                    (println "task 1 finish"))))
 
-(let [scheduler (make-scheduler)]
-  ;; Start initial threads. Thread 1 will spawn another.
-  (println "xxx"
-           @(enqueue-task scheduler
-                          (fn []
-                            (println "task 1 with id" (current-id))
-                            (let [res (enqueue-task scheduler (fn [] (println "task 2 with id" (current-id)) :fin))]
-                              (yield scheduler (current-id))
-                              (println "result?" @res))
-                            (println "task 1 finish")))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scheduler Definition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn make-scheduler
+  "Creates a scheduler that executes tasks in strict FIFO order."
+  []
+  {:new-task-queue     (chan)
+   :yielded-task-queue (chan)
+   :task-count         (atom 0)})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Task & Yield Macros - NOW AWARE OF THE TWO QUEUES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro yield! []
+  `(do
+     ;; <<< CHANGE: When yielding, the task places its control channel
+     ;; onto the :yielded-task-queue.
+     (>! (:yielded-task-queue ~'scheduler) ~'control-chan)
+
+     ;; Signal the scheduler on the return channel to unblock it.
+     (>! @~'return-chan-atom :yielded)
+
+     ;; Park and wait for the next activation.
+     (reset! ~'return-chan-atom (<! ~'control-chan))))
+
+(defmacro add-task! [scheduler & body]
+  `(let [control-chan#     (chan 1)
+         return-chan-atom# (atom nil)
+         scheduler#        ~scheduler]
+
+     ;; <<< CHANGE: A new task is always added to the :new-task-queue.
+     (go (>! (:new-task-queue scheduler#) control-chan#))
+     (swap! (:task-count scheduler#) inc)
+
+     (go
+       (let [~'control-chan control-chan#
+             ~'return-chan-atom return-chan-atom#
+             ;; Make the scheduler available to yield!
+             ~'scheduler scheduler#]
+         (try
+           (reset! ~'return-chan-atom (<! ~'control-chan))
+           ~@body
+           (>! @~'return-chan-atom :finished)
+           (catch Exception e#
+             (println "Task threw an exception:" (.getMessage e#))
+             (>! @~'return-chan-atom :finished)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scheduler Execution - NOW WITH PRIORITY
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn start-scheduler!
+  "Starts the scheduler's prioritized event loop."
+  [{:keys [new-task-queue yielded-task-queue task-count] :as scheduler}]
+  (go-loop []
+    (if (pos? @task-count)
+      (do
+        ;; <<< CRITICAL CHANGE: Use alts! with :priority
+        ;; This will check new-task-queue first. Only if it's empty/blocked
+        ;; will it check yielded-task-queue.
+        (let [[task-control-chan source-chan] (alts! [new-task-queue yielded-task-queue] :priority true)
+              return-chan (chan 1)]
+
+          ;; Activate the chosen task and wait for its response.
+          (>! task-control-chan return-chan)
+          (let [response (<! return-chan)]
+            (when (= response :finished)
+              (swap! task-count dec))))
+        (recur))
+      (println "\nAll tasks completed. Scheduler shutting down."))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Example Usage - Demonstrating FIFO Order
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn -main [& args]
+  (println "Starting FIFO cooperative scheduler...")
+
+  (let [scheduler (make-scheduler)]
+
+    (println "Adding Task A (will yield twice)")
+    (add-task! scheduler
+               (println "--> Task A: Starting first run.")
+               (yield!)
+               (println "--> Task A: Resumed for second run.")
+               (yield!)
+               (println "--> Task A: Resumed for final run, now finishing."))
+
+    (println "Adding Task B (short, no yield)")
+    (add-task! scheduler
+               (println "--> Task B: Starting and finishing."))
+
+    (println "Adding Task C (will yield once)")
+    (add-task! scheduler
+               (println "--> Task C: Starting first run.")
+               (yield!)
+               (println "--> Task C: Resumed for final run, now finishing."))
+
+    (let [done-chan (start-scheduler! scheduler)]
+      (<!! done-chan))
+
+    (println "Example finished.")))
+(-main)
