36588c4f5bf2f4f268bfd0721ee80657d874f1fd
Miguel Ping <miguel.ping@gmail.com>
2026-01-21 18:00:13 +0000

    add race

--- test/intemporal/tests/async_test.clj
+++ test/intemporal/tests/async_test.clj
@@ -10,6 +10,12 @@
   (println (str "slow activity END with " x))
   (* x 2))
 
+(defn sleep-activity [x]
+  (println (str "slow activity START with " x " on thread " (.getName (Thread/currentThread))))
+  (Thread/sleep (long x))
+  (println (str "slow activity END with " x))
+  (* x 2))
+
 ;; Parallel workflow
 (defn my-parallel-flow [id]
   (println "Workflow start with id:" id)
@@ -25,6 +31,17 @@
      :results (intemporal/join-all [prom1 prom2 prom3 prom4])
      :id      id}))
 
+;; Race workflow
+(defn my-race-flow [id]
+  (println "Workflow start with id:" id)
+  (let [sleep (intemporal/stub #'sleep-activity)
+        prom1 (intemporal/async #(sleep 100))
+        prom2 (intemporal/async #(sleep 4000))]
+    (println "After async calls - all scheduled")
+    {:race-result (intemporal/join-any [prom1 prom2])
+     :id          id}))
+
+#_
 (deftest test-async-workflow
   (testing "Async workflow"
     (intemporal/with-workflow-engine [engine {:threads 4 :enable-logging true}]
@@ -34,5 +51,18 @@
                                             my-parallel-flow [999])]
         (is (match? {:status :completed
                      :result {:args 999, :slow 0, :prom4 4, :results [2 4 6 4], :id 999}}
+                    result))))))
+
+
+(deftest test-race-workflow
+  (testing "Async race workflow"
+    (intemporal/with-workflow-engine [engine {:threads 4 :enable-logging true}]
+      ;; Register activities
+      (a/register-activity! (:registry engine) #'sleep-activity)
+      (let [result (intemporal/start-workflow engine
+                                              my-race-flow [999])]
+        (is (match? {:status :completed
+                     :result {:race-result {:index 0, :result 200}
+                              :id 999}}
                     result))))))
 
